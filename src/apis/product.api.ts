0754f0c4ab24acc6bc2d7b467a1f4b1474d476eb
catalina5 <thanhbinh12369yt@gmail.com>
2023-10-20 16:37:48 +0700

    first commit

---
+++ src/apis/product.api.ts
@@ -0,0 +1,35 @@
+import { Staff } from '~/components/Modal/CreateStaff'
+import http from '~/utils/http'
+import upload from '~/utils/upload'
+
+export const getProduct = (params?: unknown) => http.get('/product/get-all', { params })
+export const getAllKey = () => http.get('/key')
+export const blockKey = (body: { key: string; code: string }) => http.post('/key/block-key', body)
+export const removeKey = (body: { key: string }) => http.post('/key/delete-key', body)
+export const searchKey = (username: string) => http.get(`/user/auth/search?q=${username}`)
+export const searchUser = (email: string) => http.get(`/v1/user/search-staff?email=${email}`)
+export const searchComment = (title: string) => http.get(`/v1/comment/search?content=${title}`)
+export const searchContact = (title: string) => http.get(`/v1/contact/search?content=${title}`)
+export const deleteContact = (body: string[]) =>
+  http.delete(`/v1/contact/delete`, {
+    data: {
+      id: body
+    }
+  })
+export const deleteComment = (body: string[]) =>
+  http.delete(`/v1/comment/delete`, {
+    data: {
+      id: body
+    }
+  })
+export const deleteStaff = (id: string) => http.delete(`v1/user/${id}`)
+export const createKey = (body: { date: number; username: string }) => http.post('key/create', body)
+export const createStaff = (body: Staff) => http.post('v1/user/register-staff', body)
+export const updateStaff = (id: string, body: any) => upload.patch(`v1/user/${id}`, body)
+export const getAllComment = (params?: unknown) => http.get('/v1/comment/get-all-comment', { params })
+export const getAllContact = (params?: unknown) => http.get('/v1/contact/get-all-contact', { params })
+export const updateProfile = (body: any) => upload.patch('/v1/user/update', body)
+export const updateConfig = (body: { title: string; price: number; url_tele: string; content: string[] }) =>
+  http.post('/config/update', body)
+export const getAllStaff = () => http.get('v1/user/get-all-staff')
+export const repComment = (id: string, body: any) => http.post(`v1/comment/rep-comment`, body)
---
+++ src/useRouteElement.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import AdminLayout from './layouts/AdminLayout'
+import { AppContext } from './contexts/app.context'
+import RegisterLayout from './layouts/RegisterLayout'
+import Login from './pages/Login'
+import Comment from './pages/Comment'
+import Contact from './pages/Contact'
+import Users from './pages/Users'
+import Products from './pages/Product'
+import Categories from './pages/Category'
+import Oders from './pages/Order'
+import Messages from './pages/Message'
+
+function ProtecedRoute() {
+  const { isAuthenticated } = React.useContext(AppContext)
+  return isAuthenticated ? <Outlet /> : <Navigate to='login' />
+}
+function RejectedRoute() {
+  const { isAuthenticated } = React.useContext(AppContext)
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
+
+const useRouteElements = () => {
+  const routeElements = useRoutes([
+    {
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <ProtecedRoute />,
+      children: [
+        {
+          path: '/',
+          index: true,
+          element: (
+            <AdminLayout title='Dashboard'>
+              <Dashboard />
+            </AdminLayout>
+          )
+        },
+        {
+          path: '/comment',
+          element: (
+            <AdminLayout title='Danh sách bình luận'>
+              <Comment />
+            </AdminLayout>
+          )
+        },
+        {
+          path: '/contact',
+          element: (
+            <AdminLayout title='Danh sách liên hệ'>
+              <Contact />
+            </AdminLayout>
+          )
+        },
+        {
+          path: '/user',
+          element: (
+            <AdminLayout title='Danh sách nhân viên'>
+              <Users />
+            </AdminLayout>
+          )
+        },
+        {
+          path: '/product',
+          element: (
+            <AdminLayout title='Danh sách sản phẩm'>
+              <Products />
+            </AdminLayout>
+          )
+        },
+        {
+          path: '/category',
+          element: (
+            <AdminLayout title='Danh sách danh mục'>
+              <Categories />
+            </AdminLayout>
+          )
+        },
+        {
+          path: '/order',
+          element: (
+            <AdminLayout title='Danh sách đặt hàng'>
+              <Oders />
+            </AdminLayout>
+          )
+        },
+        {
+          path: '/message',
+          element: (
+            <AdminLayout title='Danh sách tin nhắn'>
+              <Messages />
+            </AdminLayout>
+          )
+        },
+      ]
+    }
+  ])
+
+  return routeElements
+}
+export default useRouteElements
